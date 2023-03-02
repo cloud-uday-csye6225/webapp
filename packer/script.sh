08f509c4f16f3db1bef54b170d7400d41bf0e97a
uday-kiran-k <113407692+uday-kiran-k@users.noreply.github.com>
2023-03-02 05:42:52 -0500

    guidelines

--- packer/script.sh
+++ packer/script.sh
@@ -8,7 +8,6 @@ echo "export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64" >>~/.bashrc
 echo "export PATH=$PATH:$JAVA_HOME/bin" >>~/.bashrc
 echo Java Location
 java --version
-sudo yum install maven -y
 echo completed Java Installation
 sudo yum install -y tomcat - y
 sudo systemctl start tomcat
