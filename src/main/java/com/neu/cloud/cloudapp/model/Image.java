b576fb2319878eb7bcbc8b0af5e0c86121333d80
uday-kiran-k <113407692+uday-kiran-k@users.noreply.github.com>
2023-03-01 22:32:19 -0500

    modified img

--- src/main/java/com/neu/cloud/cloudapp/model/Image.java
+++ src/main/java/com/neu/cloud/cloudapp/model/Image.java
@@ -6,15 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "image")
 public class Image {
